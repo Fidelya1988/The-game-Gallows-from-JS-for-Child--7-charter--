a965fc33465e7b5b3ea070a12d84440d7c86aab7
Fidelya1988 <oleksandram1988@gmail.com>
2020-12-13 00:39:31 +0200

    написала функцию, которя рисует человечка

--- Gallows.js
+++ Gallows.js
@@ -1,29 +1,101 @@
-var canvas =  document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");       //ДЯПЧИК
 ctx.strokeStyle = "Red"
 ctx.lineWidth = 3;
-ctx.beginPath();
-ctx.moveTo(50, 50); //голова
-ctx.lineTo(70, 50);
-ctx.moveTo(70, 50);
-ctx.lineTo(70, 70);
-ctx.moveTo(70, 70);
-ctx.lineTo(50, 70);
-ctx.moveTo(50, 70);
-ctx.lineTo(50, 50); //________________
-ctx.moveTo(60, 70); //тулуб
-ctx.lineTo(60, 115); //____________
-ctx.moveTo(60, 115); //ліва нога
-ctx.lineTo(70, 135);//_____________
-ctx.moveTo(60, 115); //права нога
-ctx.lineTo(50, 135);//_______
-ctx.moveTo(60, 90); //ліва рука
-ctx.lineTo(75, 80);//_____________
-ctx.moveTo(60, 90); //права нога
-ctx.lineTo(45, 80);
-ctx.stroke();
 
- 
+var unCorrectguesses = 0;
+function toDrawMan(/*unCorrectguesses*/) {
+    ctx.beginPath();
+    if (unCorrectguesses > 0) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+    }
+    if (unCorrectguesses > 1) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+    } 
+    if (unCorrectguesses > 2) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+    }
+    if (unCorrectguesses > 3) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //_____
+    }
+    if (unCorrectguesses > 4) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //________________
+        ctx.moveTo(60, 70); //тулуб
+        ctx.lineTo(60, 115); //____________
+    }
+    if (unCorrectguesses > 5) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //________________
+        ctx.moveTo(60, 70); //тулуб
+        ctx.lineTo(60, 115); //____________
+        ctx.moveTo(60, 115); //ліва нога
+        ctx.lineTo(70, 135);//_____________
+    }
+    if (unCorrectguesses > 6) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //________________
+        ctx.moveTo(60, 70); //тулуб
+        ctx.lineTo(60, 115); //____________
+        ctx.moveTo(60, 115); //ліва нога
+        ctx.lineTo(70, 135);//_____________
+        ctx.moveTo(60, 115); //права нога
+        ctx.lineTo(50, 135);//_______
+    }
+    if (unCorrectguesses > 7) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //________________
+        ctx.moveTo(60, 70); //тулуб
+        ctx.lineTo(60, 115); //____________
+        ctx.moveTo(60, 115); //ліва нога
+        ctx.lineTo(70, 135);//_____________
+        ctx.moveTo(60, 115); //права нога
+        ctx.lineTo(50, 135);//_______
+        ctx.moveTo(60, 90); //ліва рука
+        ctx.lineTo(75, 80);//_____________
+    } 
+    if (unCorrectguesses > 8) {
+        ctx.moveTo(50, 50); //голова
+        ctx.lineTo(70, 50);
+        ctx.lineTo(70, 70);
+        ctx.lineTo(50, 70);
+        ctx.lineTo(50, 50); //________________
+        ctx.moveTo(60, 70); //тулуб
+        ctx.lineTo(60, 115); //____________
+        ctx.moveTo(60, 115); //ліва нога
+        ctx.lineTo(70, 135);//_____________
+        ctx.moveTo(60, 115); //права нога
+        ctx.lineTo(50, 135);//_______
+        ctx.moveTo(60, 90); //ліва рука
+        ctx.lineTo(75, 80);//_____________
+        ctx.moveTo(60, 90); //права рука
+        ctx.lineTo(45, 80);
+    }
+    ctx.stroke();
+};
+
 var words = [
     "cat", "ara"
 ];
@@ -48,32 +120,39 @@ function getGuess() {
     return prompt("Guess a letter, or click Cancel to stop playing.");
 };
 
+var unCorrectguesses = 5; //кількість неправильних відповідей
+
 function updateGameState(guess, word, answerArray) {
-    var guessLetters = 0;
+    var guessLetters = 0; // кількість вгаданих букв у слові
     for (var j = 0; j < word.length; j++) {
 
         if (word[j] === guess && answerArray[j] === "_") {
             answerArray[j] = guess;
             guessLetters++;
-        } else if (guess === word[j].toUpperCase()) {
+        } else if (guess === word[j].toUpperCase() && answerArray[j] === "_") {
             answerArray[j] = guess.toLowerCase();
             guessLetters++;
 
-        } 
+        } else if (word[j] !== guess) {
+            //unCorrectguesses++;
+            //console.log("unCorrectguesses " + unCorrectguesses);
+
+        }
     }
+
     console.log(guessLetters);
     return guessLetters;
 };
 
 
 function showAnswerAndCongratulatePlayer(answerArray) {
-alert(answerArray.join(" ")) 
-alert("Good job! The answer was " + word);
+    alert(answerArray.join(" "))
+    alert("Good job! The answer was " + word);
 
 };
 
 var remainingLetters = word.length; //(літери, які залишились невгаданими)
-var numberOfattempts = word.length * 2; //кількість спроб
+var numberOfattempts = 3; //кількість спроб
 
 while (remainingLetters > 0 && numberOfattempts > 0) {
     alert(answerArray.join(" ")); //перетворюємо масив з рисочками на рядок
@@ -95,27 +174,4 @@ while (remainingLetters > 0 && numberOfattempts > 0) {
 
 showAnswerAndCongratulatePlayer(answerArray);
 
-var canvas =  document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = "Red"
-ctx.lineWidth = 3;
-ctx.beginPath();
-ctx.moveTo(50, 50); //голова
-ctx.lineTo(70, 50);
-ctx.moveTo(70, 50);
-ctx.lineTo(70, 70);
-ctx.moveTo(70, 70);
-ctx.lineTo(50, 70);
-ctx.moveTo(50, 70);
-ctx.lineTo(50, 50); //________________
-ctx.moveTo(60, 70); //тулуб
-ctx.lineTo(60, 115); //____________
-ctx.moveTo(60, 115); //ліва нога
-ctx.lineTo(70, 135);//_____________
-ctx.moveTo(60, 115); //права нога
-ctx.lineTo(50, 135);//_______
-ctx.moveTo(60, 90); //ліва рука
-ctx.lineTo(75, 80);//_____________
-ctx.moveTo(60, 90); //права нога
-ctx.lineTo(45, 80);
-ctx.stroke()
+toDrawMan(unCorrectguesses);
