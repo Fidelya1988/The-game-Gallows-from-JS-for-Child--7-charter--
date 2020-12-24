2631121b947187157985cc69c06e04cec1bf3b35
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2020-12-24 11:49:35 +0200

    Prettify the code
    "Почистить" код, чтобы проще было его читать.
    Я решил ничего не переименовывать, чтобы тебе проще было ориентироваться в новом коде

--- Gallows.js
+++ Gallows.js
@@ -1,8 +1,7 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");       //ДЯПЧИК
-ctx.strokeStyle = "Red"
+var ctx = canvas.getContext("2d"); //ДЯПЧИК
+ctx.strokeStyle = "Red";
 ctx.lineWidth = 3;
-
 
 function toDrawMan(unCorrectguesses) {
     ctx.beginPath();
@@ -46,7 +45,7 @@ function toDrawMan(unCorrectguesses) {
         ctx.moveTo(60, 70); //тулуб
         ctx.lineTo(60, 115); //____________
         ctx.moveTo(60, 115); //ліва нога
-        ctx.lineTo(70, 135);//_____________
+        ctx.lineTo(70, 135); //_____________
     }
     if (unCorrectguesses > 6) {
         ctx.moveTo(50, 50); //голова
@@ -57,9 +56,9 @@ function toDrawMan(unCorrectguesses) {
         ctx.moveTo(60, 70); //тулуб
         ctx.lineTo(60, 115); //____________
         ctx.moveTo(60, 115); //ліва нога
-        ctx.lineTo(70, 135);//_____________
+        ctx.lineTo(70, 135); //_____________
         ctx.moveTo(60, 115); //права нога
-        ctx.lineTo(50, 135);//_______
+        ctx.lineTo(50, 135); //_______
     }
     if (unCorrectguesses > 7) {
         ctx.moveTo(50, 50); //голова
@@ -70,11 +69,11 @@ function toDrawMan(unCorrectguesses) {
         ctx.moveTo(60, 70); //тулуб
         ctx.lineTo(60, 115); //____________
         ctx.moveTo(60, 115); //ліва нога
-        ctx.lineTo(70, 135);//_____________
+        ctx.lineTo(70, 135); //_____________
         ctx.moveTo(60, 115); //права нога
-        ctx.lineTo(50, 135);//_______
+        ctx.lineTo(50, 135); //_______
         ctx.moveTo(60, 90); //ліва рука
-        ctx.lineTo(75, 80);//_____________
+        ctx.lineTo(75, 80); //_____________
     }
     if (unCorrectguesses > 8) {
         ctx.moveTo(50, 50); //голова
@@ -85,74 +84,59 @@ function toDrawMan(unCorrectguesses) {
         ctx.moveTo(60, 70); //тулуб
         ctx.lineTo(60, 115); //____________
         ctx.moveTo(60, 115); //ліва нога
-        ctx.lineTo(70, 135);//_____________
+        ctx.lineTo(70, 135); //_____________
         ctx.moveTo(60, 115); //права нога
-        ctx.lineTo(50, 135);//_______
+        ctx.lineTo(50, 135); //_______
         ctx.moveTo(60, 90); //ліва рука
-        ctx.lineTo(75, 80);//_____________
+        ctx.lineTo(75, 80); //_____________
         ctx.moveTo(60, 90); //права рука
         ctx.lineTo(45, 80);
     }
     ctx.stroke();
-};
-
+}
 
 function pickWord() {
     return words[Math.floor(Math.random() * words.length)];
-
-}; // отримуємо слово з масиву з відповідями
-
+} // отримуємо слово з масиву з відповідями
 
 function setupAnswerArray(word) {
     var answerOfArray = [];
     for (var i = 0; i < word.length; i++) {
         answerOfArray[i] = "_";
-
     }
     return answerOfArray;
-};
-
-
+}
 
 function getGuess() {
     return prompt("Guess a letter, or click Cancel to stop playing.");
-};
-
+}
 
 function updateGameState(guess, word, answerArray) {
     var guessLetters = 0; // кількість вгаданих букв у слові
     for (var j = 0; j < word.length; j++) {
-
         if (word[j] === guess && answerArray[j] === "_") {
             answerArray[j] = guess;
             guessLetters++;
         } else if (guess === word[j].toUpperCase() && answerArray[j] === "_") {
             answerArray[j] = guess.toLowerCase();
             guessLetters++;
-
         } else if (word[j] !== guess) {
-
-
         }
     }
 
-
     return guessLetters;
-};
-
+}
 
 function showAnswerAndCongratulatePlayer(answerArray) {
     if (unCorrectguesses >= 9) {
         alert("Sorry, you lost : - ( The answer was " + word);
     } else {
-
         alert(answerArray.join(" "));
         alert("Good job! The answer was " + word);
     }
-};
-var words = [
-    "cat", "ara", "flamingo"
-];
+}
+
+var words = ["cat", "ara", "flamingo"];
 var word = pickWord();
 var answerArray = setupAnswerArray(word);
 var remainingLetters = word.length; //(літери, які залишились невгаданими)
@@ -163,39 +147,27 @@ setTimeout(function unCorrectGuesses() {
         alert(answerArray.join(" ")); //перетворюємо масив з рисочками на рядок
         var guess = getGuess();
 
-
-
         /*(введена літера)*/
         if (guess === null) {
             var stopThegame = confirm("Do you want to stop the game?");
             if (!stopThegame) {
-
-            setTimeout(unCorrectGuesses, 500);
-        }
+                setTimeout(unCorrectGuesses, 500);
+            }
         } else if (guess.length !== 1) {
-            alert("Please enter a single letter")
+            alert("Please enter a single letter");
             setTimeout(unCorrectGuesses, 500);
         } else {
-
             var correctGuesses = updateGameState(guess, word, answerArray);
             remainingLetters -= correctGuesses;
             if (correctGuesses === 0) {
-
-                unCorrectguesses++
+                unCorrectguesses++;
                 toDrawMan(unCorrectguesses);
                 setTimeout(unCorrectGuesses, 500);
             } else {
                 setTimeout(unCorrectGuesses, 500);
             }
-
         }
-
     } else {
-
-
-
         showAnswerAndCongratulatePlayer(answerArray);
     }
 }, 0);
-
-
